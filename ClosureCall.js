ae5f62ccdeb88c1b891f19b7108ee75123a4df9f
iamajjayya <ajjayya2002@gmail.com>
2024-06-19 13:04:49 +0530

    update closure function using var

--- ClosureCall.js
+++ ClosureCall.js
@@ -1,12 +1,56 @@
 
 
-function x() {
-       for (let i=0; i<=5; i++) {
-        setTimeout( function () {
-            console.log(i)
-        }, i * 1000)
-       }
+// // function x() {
+// //        for (let i=0; i<=5; i++) {
+// //         setTimeout( function () {
+// //             console.log(i)
+// //         }, i * 1000)
+// //        }
 
+// // }
+
+// // x()
+
+// function x() {
+//    for (var i = 1 ;  i<=5 ; i++)
+//     {
+
+//           function close(x) {
+//                setTimeout (function () {
+//                 console.log(x);
+//                }, x * 1000)
+//           }
+//     }
+
+//     close(i);
+
+// }
+
+// x();
+
+function x() {
+    for (var i = 1; i <= 5; i++) {
+        (function (x) { // IIFE to capture the current value of i
+            setTimeout(function () {
+                console.log(x);
+            }, x * 1000);
+        })(i); // Immediately invoke the function with the current value of i
+    }
 }
 
-x()
+x();
+
+
+function y() {
+      for(var  i =1;  i<=5; i++) {
+        (
+            function (x) {
+                 setTimeout( function () {
+                    console.log(x)
+                 }, x * 1000)
+            }
+        )(i);
+      }
+}
+
+y();
