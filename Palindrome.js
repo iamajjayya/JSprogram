6e68054a0bb3aae62db8f0c8d0a88296dee5fa31
iamajjayya <ajjayya2002@gmail.com>
2024-07-17 08:53:55 +0530

    palindrome

--- Palindrome.js
+++ Palindrome.js
@@ -1,20 +1,30 @@
-function isPalindrome(str) {
+// function isPalindrome(str) {
 
-    str = str.toLowerCase().replace(/[^a-z0-9]/g,'')
+//     str = str.toLowerCase().replace(/[^a-z0-9]/g,'')
 
-    let reverse = str.split('').reverse().join('')
-    console.log(str, reverse)
+//     let reverse = str.split('').reverse().join('')
+//     console.log(str, reverse)
 
 
-    if (str  === reverse){
-        return " Its Palindrome"
-    } else {
-        return "Its not a  Palindorme"
-    }
+//     if (str  === reverse){
+//         return " Its Palindrome"
+//     } else {
+//         return "Its not a  Palindorme"
+//     }
 
 
     
+// }
+
+// console.log(isPalindrome("Ajjayya"))
+
+
+function isPalindrome(num) {
+    const str  =  num.toString();
+
+    const reversdnum  = str.split('').reverse().join('');
+
+    return reversdnum === str;
 }
 
-console.log(isPalindrome("Ajjayya"))
-
+console.log(isPalindrome(121))
